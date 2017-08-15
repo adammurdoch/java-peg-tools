7d2bbfa8c072d39722d7a9a049acc20c49dc846b
Adam Murdoch <adam@gradle.com>
2017-08-15 18:08:59 +1000

    Added a rough GUI to the test app to allow parsing to be manually tested.

--- testapp/src/main/java/net/rubygrapefruit/parser/java/test/Main.java
+++ testapp/src/main/java/net/rubygrapefruit/parser/java/test/Main.java
@@ -3,13 +3,30 @@ package net.rubygrapefruit.parser.java.test;
 import net.rubygrapefruit.parser.java.JavaParser;
 import net.rubygrapefruit.parser.peg.TokenVisitor;
 
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        if (args.length == 1 && args[0].equals("--gui")) {
+            showGui();
+        } else {
+            parseFiles(args);
+        }
+    }
+
+    private static void parseFiles(String[] args) throws IOException {
         JavaParser parser = new JavaParser();
         for (String arg : args) {
             File file = new File(arg);
@@ -43,6 +60,119 @@ public class Main {
             });
             System.out.println("-------");
             System.out.println();
+        }
+    }
+
+    private static void showGui() {
+        final JTextArea source = new JTextArea();
+        final JTextArea parsed = new JTextArea();
+        source.getDocument().addDocumentListener(new TextChangeHandler(parsed, source));
+        parsed.setEditable(false);
+        JSplitPane pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, new JScrollPane(source), new JScrollPane(parsed));
+        pane.setResizeWeight(0.5);
+
+        JPanel root = new JPanel();
+        root.setLayout(new BorderLayout());
+        root.add(pane, BorderLayout.CENTER);
+
+        JFrame frame = new JFrame("Parser test");
+        frame.setContentPane(root);
+        frame.setSize(1000, 800);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    private static class TextChangeHandler implements DocumentListener {
+        private final JTextArea source;
+        private final Executor executor = Executors.newSingleThreadExecutor();
+        private final Lock lock = new ReentrantLock();
+        private final Condition condition = lock.newCondition();
+        private final JavaParser javaParser = new JavaParser();
+        private String current;
+
+        TextChangeHandler(final JTextArea parsed, JTextArea source) {
+            this.source = source;
+            executor.execute(new ParseLoop(parsed));
+        }
+
+        @Override
+        public void insertUpdate(DocumentEvent e) {
+            onUpdate();
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent e) {
+            onUpdate();
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent e) {
+            onUpdate();
+        }
+
+        private void onUpdate() {
+            lock.lock();
+            try {
+                current = source.getText();
+                condition.signalAll();
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        private class ParseLoop implements Runnable {
+            private final JTextArea parsed;
+
+            ParseLoop(JTextArea parsed) {
+                this.parsed = parsed;
+            }
+
+            @Override
+            public void run() {
+                while (true) {
+                    doOnce();
+                }
+            }
+
+            private void doOnce() {
+                String str;
+                lock.lock();
+                try {
+                    while (current == null) {
+                        try {
+                            condition.await();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    str = current;
+                    current = null;
+                } finally {
+                    lock.unlock();
+                }
+
+                final StringBuilder builder = new StringBuilder();
+                javaParser.parse(str, new TokenVisitor() {
+                    @Override
+                    public void token(String token) {
+                        builder.append(token);
+                    }
+
+                    @Override
+                    public void failed(String message) {
+                        builder.append("\n\n");
+                        builder.append("FAILED: ");
+                        builder.append(message);
+                    }
+                });
+
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        parsed.setText(builder.toString());
+                    }
+                });
+            }
         }
     }
 }
